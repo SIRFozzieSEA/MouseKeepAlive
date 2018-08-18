6607ba2eac205c9585f7433edca197c0f428769c
SIRFozzieSEA <github@fozden.com>
2018-08-17 18:20:58 -0700

    Put log4J properties in the right folder. Removed old log formatter.

--- src/main/java/com/codef/mousekeepalive/MouseKeepAlive.java
+++ src/main/java/com/codef/mousekeepalive/MouseKeepAlive.java
@@ -14,12 +14,9 @@ public class MouseKeepAlive {
 
 	private static final Logger LOGGER = Logger.getLogger(MouseKeepAlive.class.getName());
 	
-	static {
-		System.setProperty("java.util.logging.SimpleFormatter.format",
-				"%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS %4$s %2$s %5$s%6$s%n");
-	}
-
 	public static void main(String[] args) {
+		
+		LOGGER.info("Started");
 
 		Properties defaultProps = new Properties();
 		try {
